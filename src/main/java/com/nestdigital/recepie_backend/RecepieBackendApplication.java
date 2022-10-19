954d4bbabdc898e56d6f926a5193114292dde2c8
devika0999 <krishnadevika445@gmail.com>
2022-10-19 18:59:13 +0530

    recepie

---
+++ src/main/java/com/nestdigital/recepie_backend/RecepieBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.recepie_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecepieBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/recepie_backend/RecepieBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.recepie_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepieBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
